72d4ac8e9b92f8e53a07d7c1fce9e2413c37d8a5
fedeb123 <federicoab@gmail.com>
2026-04-19 23:25:38 -0300

    Adicion de pantalla de reservas

---
+++ app/src/main/java/com/example/da1androidnative/ui/home/ReservasFragment.java
@@ -0,0 +1,27 @@
+package com.example.da1androidnative.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.da1androidnative.R;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
+public class ReservasFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_reservas, container, false);
+    }
+}
+
