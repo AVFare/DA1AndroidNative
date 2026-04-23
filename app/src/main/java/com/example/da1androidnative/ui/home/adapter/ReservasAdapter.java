8d7443a3f921734f97c05fe9a33c3f28797a735c
fedeb123 <federicoab@gmail.com>
2026-04-22 22:33:22 -0300

    completado de logica del adapter de reserva

--- app/src/main/java/com/example/da1androidnative/ui/home/adapter/ReservasAdapter.java
+++ app/src/main/java/com/example/da1androidnative/ui/home/adapter/ReservasAdapter.java
@@ -1,11 +1,13 @@
 package com.example.da1androidnative.ui.home.adapter;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import com.example.da1androidnative.R;
 import com.example.da1androidnative.data.model.ReservaResponse;
+import com.google.android.material.card.MaterialCardView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -25,6 +27,7 @@ public class ReservasAdapter extends RecyclerView.Adapter<ReservasAdapter.Reserv
         TextView reservationParticipants;
         TextView reservationVoucherCode;
         TextView reservationId;
+        MaterialCardView cardView;
 
         public ReservasViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -36,35 +39,63 @@ public class ReservasAdapter extends RecyclerView.Adapter<ReservasAdapter.Reserv
             reservationParticipants = itemView.findViewById(R.id.reservationParticipants);
             reservationVoucherCode = itemView.findViewById(R.id.reservationVoucherCode);
             reservationId = itemView.findViewById(R.id.reservationId);
-
+            cardView = itemView.findViewById(R.id.reservationCard);
         }
 
     }
 
-    private final List<ReservaResponse> reservas = new ArrayList<>();
+    private List<ReservaResponse> reservas = new ArrayList<>();
+
+    private final Context context;
+
+    private OnReservaClickListener listener;
+
+    public interface OnReservaClickListener {
+        void onReservaClick(Long reservationId);
+
+    }
+
+    public ReservasAdapter(Context context, OnReservaClickListener listener) {
+        this.context = context;
+        this.listener = listener;
+    }
+    public void setReservas(List<ReservaResponse> reservas) {
+        this.reservas = reservas;
+        notifyDataSetChanged();
+    }
 
     @Override
     public int getItemCount() {
         return reservas.size();
     }
 
+    @NonNull
     @Override
-    public ReservasViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_reserva, parent, false);
+    public ReservasViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_reserva, parent, false);
         return new ReservasViewHolder(view);
 
     }
 
     @Override
     public void onBindViewHolder(ReservasViewHolder holder, int position) {
-        holder.reservationActivityName.setText(reservas.get(position).getActivityName());
-        holder.reservationStatus.setText(reservas.get(position).getStatus());
-        holder.reservationDestination.setText(reservas.get(position).getDestination());
-        holder.reservationDate.setText(reservas.get(position).getDate().toString());
-        holder.reservationTime.setText(reservas.get(position).getTime());
-        holder.reservationParticipants.setText(reservas.get(position).getParticipantsCount());
-        holder.reservationVoucherCode.setText(reservas.get(position).getVoucherCode());
-        holder.reservationId.setText(reservas.get(position).getReservationId());
+
+        ReservaResponse reserva = reservas.get(position);
+
+        holder.reservationActivityName.setText(reserva.getActivityName());
+        holder.reservationStatus.setText(reserva.getStatus());
+        holder.reservationDestination.setText(reserva.getDestination());
+        holder.reservationDate.setText(reserva.getDate().toString());
+        holder.reservationTime.setText(reserva.getTime());
+        holder.reservationParticipants.setText(String.format("Cantidad de Participantes: %d", reserva.getParticipantsCount()));
+        holder.reservationVoucherCode.setText(reserva.getVoucherCode());
+        holder.reservationId.setText(String.format("ID Reserva: %d", reserva.getReservationId()));
+
+        holder.cardView.setOnClickListener(v -> {
+            if (listener != null) {
+                listener.onReservaClick(reserva.getReservationId());
+            }
+        });
     }
 
 
