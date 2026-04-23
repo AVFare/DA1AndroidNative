e7206efd2d22cc8ff4056600b74fbcbec90abb23
fedeb123 <federicoab@gmail.com>
2026-04-22 22:33:06 -0300

    implementacion de logica para el fragment de reserva

--- app/src/main/java/com/example/da1androidnative/ui/home/ReservasFragment.java
+++ app/src/main/java/com/example/da1androidnative/ui/home/ReservasFragment.java
@@ -4,23 +4,39 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.da1androidnative.R;
+import com.example.da1androidnative.data.model.PaginatedReservasResponse;
+import com.example.da1androidnative.data.model.ReservaResponse;
 import com.example.da1androidnative.data.network.ApiService;
+import com.example.da1androidnative.ui.home.adapter.ReservasAdapter;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
 import dagger.hilt.android.AndroidEntryPoint;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 @AndroidEntryPoint
-public class ReservasFragment extends Fragment {
+public class ReservasFragment extends Fragment implements ReservasAdapter.OnReservaClickListener {
 
     @Inject
     ApiService apiService;
+
+    private ReservasAdapter reservasAdapter;
+
+    private RecyclerView recyclerView;
 
     @Nullable
     @Override
@@ -28,6 +44,50 @@ public class ReservasFragment extends Fragment {
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_reservas, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        setupRecyclerView(view);
+        loadReservas();
+    }
+
+    private void setupRecyclerView(View view) {
+        recyclerView = view.findViewById(R.id.reservasRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        reservasAdapter = new ReservasAdapter(getContext(), this);
+        recyclerView.setAdapter(reservasAdapter);
+    }
+
+
+    private void loadReservas() {
+        apiService.getAllReservas().enqueue(new Callback<PaginatedReservasResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<PaginatedReservasResponse> call, @NonNull Response<PaginatedReservasResponse> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    List<ReservaResponse> listaReservas = response.body().getContent();
+                    android.util.Log.d("API_HOME", "Reservas extraídas del paginado: " + listaReservas.size());
+                    reservasAdapter.setReservas(listaReservas);
+                } else {
+                    android.util.Log.e("API_HOME", "Error en respuesta: " + response.code());
+                    Toast.makeText(getContext(), "Error al cargar reservas", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<PaginatedReservasResponse> call, Throwable t) {
+                android.util.Log.e("API_HOME", "Fallo total: " + t.getMessage());
+                Toast.makeText(getContext(), "Error de red: " + t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public void onReservaClick(Long reservationId) {
+        Toast.makeText(getContext(), "ID Reserva: " + reservationId, Toast.LENGTH_SHORT).show();
+        //TODO: Navegacion hacia detalle de reserva
     }
 }
 
