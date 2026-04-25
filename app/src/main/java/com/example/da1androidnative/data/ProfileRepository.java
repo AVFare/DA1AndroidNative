5b2b2313e899dd80db66597ae390d7cbe136e07c
AVFare <118075989+AVFare@users.noreply.github.com>
2026-04-25 20:08:57 -0300

    PERFIL DEL VIAJERO: -  ProfileFragment: se agrega fragment de visualización de perfil VERSIÓN 1. - ProfileRepository: se genera repositorio de perfil. - ProfileViewModel: se agrega el View Model del perfil VERSIÓN 1.

---
+++ app/src/main/java/com/example/da1androidnative/data/ProfileRepository.java
@@ -0,0 +1,29 @@
+package com.example.da1androidnative.data;
+
+import com.example.da1androidnative.data.model.UpdateUserProfileRequest;
+import com.example.da1androidnative.data.model.UserProfileResponse;
+import com.example.da1androidnative.data.network.ApiService;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import retrofit2.Call;
+
+@Singleton
+public class ProfileRepository {
+
+    private final ApiService apiService;
+
+    @Inject
+    public ProfileRepository(ApiService apiService) {
+        this.apiService = apiService;
+    }
+
+    public Call<UserProfileResponse> getMyProfile() {
+        return apiService.getMyProfile();
+    }
+
+    public Call<UserProfileResponse> updateMyProfile(UpdateUserProfileRequest request) {
+        return apiService.updateMyProfile(request);
+    }
+}
