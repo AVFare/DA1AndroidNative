13e0c92017886861ddf7c2b9430860a135d4501e
Jordii05 <jordicl2018@gmail.com>
2026-04-28 22:27:27 -0300

    funcionalidades iniciales

--- app/src/main/java/com/example/da1androidnative/ui/home/NewsFragment.java
+++ app/src/main/java/com/example/da1androidnative/ui/home/NewsFragment.java
@@ -1,4 +1,34 @@
 package com.example.da1androidnative.ui.home;
 
-public class NewsFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import com.example.da1androidnative.R;
+
+public class NewsFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_news, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ImageButton btnBack = view.findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(v -> {
+            Navigation.findNavController(view).navigateUp();
+        });
+
+
+    }
 }
