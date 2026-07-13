9e4022d8e389e5af42c84684a1b43cc31f9548a2
Jordii05 <jordicl2018@gmail.com>
2026-07-13 00:18:00 -0300

    eliminacion de dependencia circular, refactorizacion

--- app/src/main/java/com/example/da1androidnative/data/local/NotificationStorage.java
+++ app/src/main/java/com/example/da1androidnative/data/local/NotificationStorage.java
@@ -24,21 +24,30 @@ public class NotificationStorage {
         this.prefs = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
         this.gson = new Gson();
     }
-    public synchronized boolean saveNotification(Notification notification) {
+
+    private String getUserKey(long userId) {
+        return KEY_NOTIFS + userId;
+    }
+
+    public synchronized boolean saveNotification(long userId, Notification notification) {
         if (notification == null || notification.getId() == null) return false;
-        List<Notification> list = getNotifications();
+        if (userId == -1) return false;
+
+        List<Notification> list = getNotifications(userId);
         
         for (Notification n : list) {
             if (notification.getId().equals(n.getId())) return false;
         }
         
         list.add(0, notification); // la mas reciente al principio
-        saveList(list);
+        saveList(userId,list);
         return true;
     }
 
-    public synchronized List<Notification> getNotifications() {
-        String json = prefs.getString(KEY_NOTIFS, null);
+    public synchronized List<Notification> getNotifications(long userId) {
+        if (userId == -1) return new ArrayList<>();
+        
+        String json = prefs.getString(getUserKey(userId), null);
         if (json == null) return new ArrayList<>();
         Type type = new TypeToken<List<Notification>>() {}.getType();
         try {
@@ -49,15 +58,19 @@ public class NotificationStorage {
         }
     }
 
-    public synchronized void removeNotification(Long id) {
-        if (id == null) return;
-        List<Notification> list = getNotifications();
+    public synchronized void removeNotification(long userId,Long id) {
+        if (id == null || userId == -1) return;
+        List<Notification> list = getNotifications(userId);
         list.removeIf(n -> id.equals(n.getId()));
-        saveList(list);
+        saveList(userId,list);
     }
 
-    private void saveList(List<Notification> list) {
+    public synchronized void clearAll(long userId) {
+        prefs.edit().remove(getUserKey(userId)).apply();
+    }
+
+    private void saveList(long userId, List<Notification> list) {
         String json = gson.toJson(list);
-        prefs.edit().putString(KEY_NOTIFS, json).apply();
+        prefs.edit().putString(getUserKey(userId), json).apply();
     }
 }
